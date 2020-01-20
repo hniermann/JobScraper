45824d7552c560f04071adb66a7f26bd1b5ed0d0
Henry Niermann <henryniermann4@gmail.com>
2020-01-19 23:04:54 -0500

    Added selenium capabilities

--- com/company/JobScraper.java
+++ com/company/JobScraper.java
@@ -7,17 +7,33 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
 public class JobScraper {
 
     public static void main(String[] args) {
-        try {
-            // fetch the document over HTTP
-            Document doc = Jsoup.connect("https://www.indeed.com/q-Software-Engineer-jobs.html").get();
+//
+            System.setProperty("webdriver.chrome.driver","C:\\Users\\henry\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+            WebDriver driver = new ChromeDriver();
+            driver.get("https://www.indeed.com");
 
-            // get the page title
-            String title = doc.title();
-            System.out.println("title: " + title);
+            String title = driver.getTitle();
+            System.out.println(title);
 
+            WebElement searchBox = driver.findElement(By.id("text-input-what"));
+            searchBox.sendKeys("Software Engineering Intern");
+            WebElement searchClick = driver.findElement(By.tagName("button"));
+            searchClick.click();
+
+            String url = driver.getCurrentUrl();
+
+            try{
+                Document doc = Jsoup.connect(url).get();
+                System.out.println("title: "+doc.title());
 
             Elements links = doc.select("a[target]");
             for(Element link : links){
@@ -31,7 +47,7 @@ public class JobScraper {
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+//            e.printStackTrace();
         }
     }
 }
